f43b6880808baef4f25641a6fc59f11b8f62546a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2025-09-01 06:15:50 -0300

    refac: remove test_users

--- main.py
+++ main.py
@@ -22,20 +22,6 @@ def read_root():
     return {"message": "Health check: success."}
 
 
-@app.post("/test_users/", response_model=schemas.UserBase, status_code=201)
-def create_test_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = models.UserTest(name=user.name, email=user.email)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-@app.get("/test_users/", response_model=list[schemas.UserBase])
-def read_test_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = db.query(models.UserTest).offset(skip).limit(limit).all()
-    return users
-
-
 @app.post("/users/login", response_model=schemas.UserBase)
 def find_or_create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
