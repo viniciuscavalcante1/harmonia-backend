9e732ad9c9e871174cd4980cfa4fbe84f7e70299
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2025-09-06 12:32:17 -0300

    style: ruff

--- app/schemas.py
+++ app/schemas.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel, EmailStr
 from datetime import date
 from typing import List
 
+
 class HabitBase(BaseModel):
     name: str
     icon: str
     date: date
     is_completed: bool
+
 
 class HabitCreate(BaseModel):
     name: str
@@ -26,8 +28,10 @@ class UserBase(BaseModel):
     name: str
     email: EmailStr
 
+
 class UserCreate(UserBase):
     pass
+
 
 class User(UserBase):
     id: int
@@ -36,11 +40,14 @@ class User(UserBase):
     class Config:
         from_attributes = True
 
+
 class ActivityData(BaseModel):
     steps: int
 
+
 class SleepData(BaseModel):
     duration: str
+
 
 class DashboardDataResponse(BaseModel):
     userName: str
